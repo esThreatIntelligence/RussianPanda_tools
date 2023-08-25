fcbede62995dbae26b9c131e16eb4d4498415e35
RussianPanda <91575783+RussianPanda95@users.noreply.github.com>
2023-08-25 09:07:07 +0500

    Add files via upload

---
+++ lummac2_fetch_config_from_C2.py
@@ -0,0 +1,33 @@
+import requests
+import base64
+import json  
+
+session = requests.Session()
+
+# Proxies setup
+session.proxies = {
+    'http': '198.49.68.80:80',
+    'https': '198.49.68.80:80',
+}
+
+url = input("Please enter the URL to fetch config from (for example: http://URL/c2conf): ")
+
+try:
+    response = session.get(url, timeout=10)  # Setting the timeout to 10 seconds
+
+    def xor_decrypt(data, key):
+        return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
+
+    decoded_data = base64.b64decode(response.text)
+    key = decoded_data[:32]
+
+    decrypted_data = xor_decrypt(decoded_data[32:], key)
+
+    decoded_string = decrypted_data.decode('utf-8', errors='replace')  
+
+    data_beautified = json.dumps(json.loads(decoded_string), indent=4)
+
+    print(data_beautified)
+
+except requests.exceptions.Timeout:
+    print("Server is likely down")
